7e884d4fcd629f1aa07526ca052795374c588e8e
Federico Sassone <fsassone@ws8.labo1>
2016-08-18 20:08:07 -0300

    faltantres

--- Orga2/tp1/solucion/cuatrotree.asm
+++ Orga2/tp1/solucion/cuatrotree.asm
@@ -16,7 +16,7 @@
 
 ; DEFINES
   %define null					0
-  %define tamanio_0 			0
+  %define tamanio_ajustar_pila	8
 
   %define pointer_SIZE 			8
   %define double_int_SIZE		4		
@@ -26,6 +26,16 @@
 
   %define arbol_root_OFFSET 	0
   %define arbol_size_OFFSET		8
+  %define nodo_padre_OFFSET		0
+  %define nodo_value0_OFFSET	8
+  %define nodo_value1_OFFSET	12
+  %define nodo_value2_OFFSET	16
+  %define nodo_len_OFFSET		20
+  %define nodo_hijo0_OFFSET		21
+  %define nodo_hijo1_OFFSET		29
+  %define nodo_hijo2_OFFSET		37
+  %define nodo_hijo3_OFFSET		45
+
   %define iter_tree_OFFSET		0
   %define iter_node_OFFSET		8
   %define iter_current_OFFSET 	16
@@ -49,7 +59,7 @@ ct_new:
 			call malloc
 			mov rdi, rax ; rdi = puntero a memoria para el str_arbol_SIZE
 			mov qword [rdi + arbol_root_OFFSET], null
-			mov dword [rdi + arbol_size_OFFSET], tamanio_0
+			mov dword [rdi + arbol_size_OFFSET], null
 			mov r9, rdi
 		pop rbp
         ret
@@ -57,6 +67,22 @@ ct_new:
 ; =====================================
 ; void ct_delete(ctTree** pct);
 ct_delete:
+		push rbp
+		mov rsp, rbp
+		push rbx
+		push r12
+		push r13
+		push r14
+			mov rbx, rdi ; rbx=**tree
+			mov r12, [rbx];r12=*tree
+			cmp qword [r12+arbol_root_OFFSET], null
+			je .fin
+			mov r9, [r8 + arbol_root_OFFSET]
+
+			.fin:
+			mov rdi, r8
+			call free
+		pop rbp
         ret
 
 ; ; =====================================
@@ -74,15 +100,19 @@ ct_print:
 ctIter_new:
 		push rbp
 		mov rsp, rbp
+		push rbx
+		sub rsp, tamanio_ajustar_pila
+			xor rbx, rbx
 			mov rbx, rdi
 			mov rdi, str_iter_SIZE
 			xor rax, rax
 			call malloc
-			mov qword [rax + iter_tree_OFFSET], rbx
-			mov qword [rax + iter_node_OFFSET], tamanio_0
-			mov byte  [rax + iter_current_OFFSET], tamanio_0
-			mov dword [rax + iter_count_OFFSET], tamanio_0
-
+			mov [rax + iter_tree_OFFSET], rbx
+			mov qword [rax + iter_node_OFFSET], null
+			mov byte  [rax + iter_current_OFFSET], null
+			mov dword [rax + iter_count_OFFSET], null
+		add rsp, tamanio_ajustar_pila
+		pop rbx	
 		pop rbp
         ret
 
@@ -91,13 +121,44 @@ ctIter_new:
 ctIter_delete:
 		push rbp
 		mov rsp, rbp
-			free rdi
+			call free 
 		pop rbp
         ret
 
 ; =====================================
 ; void ctIter_first(ctIter* ctIt);
 ctIter_first:
+		;tengo en rdi el puntero al iter de la forma: |*arbol|*nodo actual|byte valorActual|double Count|
+		; arbol es de la forma: |nodo* raiz|double tamanio|
+		; nodo es: |nodo* padre| double[3] valores | byte longitud | nodo*[4] hijos|
+		push rbp
+		mov rsp, rbp
+		push rbx
+		push r12
+			cmp qword [rdi + iter_tree_OFFSET], null
+			je .fin
+			mov rbx, [rdi+ iter_tree_OFFSET]; en rbx el puntero al arbol
+			cmp qword [rdi + arbol_root_OFFSET], null
+			je .fin 
+			mov r12, [rbx + arbol_root_OFFSET]; rn r12 el nodo raiz del arbol
+			cmp byte [r12 + nodo_len_OFFSET], null
+			je .fin
+			.cicloBuscoMenor:
+			;xor rsi, rsi
+			;mov esi, [r12 + nodo_value0_OFFSET]; tomamos el menor valor
+			cmp qword [r12 + nodo_hijo0_OFFSET], null
+			je .menorNodo
+			mov r12,  [r12 + nodo_hijo0_OFFSET]
+			jmp .cicloBuscoMenor
+			.menorNodo: ; entramos aca si estamos en el nodo de abajo a la izq de todo
+			mov [rdi + iter_tree_OFFSET], rbx ; el arbol 
+			mov [rdi + iter_node_OFFSET], r12 ; el nodo
+			mov byte  [rdi + iter_current_OFFSET], null; el current 
+			mov dword [rdi + iter_count_OFFSET], null ;contador 
+		.fin:
+		pop r12
+		pop rbx	
+		pop rbp
         ret
 
 ; =====================================
@@ -108,11 +169,31 @@ ctIter_next:
 ; =====================================
 ; uint32_t ctIter_get(ctIter* ctIt);
 ctIter_get:
+		push rbp
+		mov rsp, rbp
+			xor rax, rax
+			xor rcx, rcx
+			mov r8, [rdi + iter_node_OFFSET]
+			mov cl, [rdi + iter_current_OFFSET]
+			lea r9, [r8 + nodo_value0_OFFSET]
+			add r9, rcx
+			mov rax, r9
+		pop rbp
         ret
 
 ; =====================================
 ; uint32_t ctIter_valid(ctIter* ctIt);
 ctIter_valid:
+		push rbp
+		mov rsp, rbp
+			cmp qword [rdi + iter_node_OFFSET], null
+			je .notValid
+			mov rax, 1
+			jmp .fin
+			.notValid:
+			xor rax, rax
+			.fin:
+		pop rbp
         ret
 
 
--- Orga2/tp1/solucion/cuatrotree.c
+++ Orga2/tp1/solucion/cuatrotree.c
@@ -1,14 +1,25 @@
 #include "cuatrotree.h"
 
 void ct_add(ctTree* ct, uint32_t newVal) {
-
+	//agregamos un elemento a un nodo del arbol o creamos un nodo con ese elem
+	//faltan func de iter
+	//ctNode = ct_aux_search()
 
 }
 
 	ctNode* ct_aux_search(ctNode** currNode, ctNode* fatherNode, uint32_t newVal){
-
+		//busca un nodo donde agregar el elemento y lo devuelve, si no lo genera, lo conecta y devuelve
+		/*
+		typedef struct ctNode_t {
+		  struct ctNode_t* father;
+		  uint32_t value[NODESIZE];
+		  uint8_t len;
+		  struct ctNode_t* child[NODESIZE+1];
+		} __attribute__((__packed__)) ctNode;
+	*/
 	}	
 
 	void ct_aux_fill(ctNode* currNode, uint32_t newVal){
+		//dado un nodo, agrega ordenadamente el valor haciendo los movimientos necesarios y aumentando el tam del nodo
 		
 	}
