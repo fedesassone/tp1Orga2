341279971af40a79bcbd8479d661b45ee2156393
Jazz <jazminalvarezvico@gmail.com>
2016-09-04 22:57:14 -0300

    asd

--- Orga2/tp1/solucion/cuatrotree.c
+++ Orga2/tp1/solucion/cuatrotree.c
@@ -15,95 +15,74 @@ void ct_add(ctTree* ct, uint32_t newVal) {
 
 ctNode* ct_aux_search(ctNode** currNode, ctNode* fatherNode, uint32_t newVal){
 	ctNode* actual = (*currNode);
-	if (actual->len!=3) return actual;
+	if (actual->len<3) return actual;
 	//for(int i=0;i<3;i++){
 	if (actual->value[0]>newVal){
 		if(actual->child[0]!=NULL)
 			return ct_aux_search(&(actual->child[0]),actual,newVal);
 		ctNode *nuevo = ct_nuevoNodo(actual);
-		return nuevo;
-	}
-	if(actual->value[2]<newVal){
-		if(actual->child[3]!=NULL)
-			return ct_aux_search(&(actual->child[3]),actual,newVal);
-		ctNode *nuevo = ct_nuevoNodo(actual);
+		actual->child[0]=nuevo;
 		return nuevo;
 	}
 	if(actual->value[0]<newVal && actual->value[1]>newVal){
 		if(actual->child[1]!=NULL)
 			return ct_aux_search(&(actual->child[1]),actual,newVal);
 		ctNode *nuevo = ct_nuevoNodo(actual);
+		actual->child[1]=nuevo;
 		return nuevo;
 	}
 	if(actual->value[1]<newVal && actual->value[2]>newVal){
 		if(actual->child[2]!=NULL)
 			return ct_aux_search(&(actual->child[2]),actual,newVal);
 		ctNode *nuevo = ct_nuevoNodo(actual);
+		actual->child[2]=nuevo;
 		return nuevo;
 	}
-/*
-	for(int i=0;i<3;i++){
-		if((*currNode)->value[i]>newVal){
-			if((*currNode)->value[i]!=0){
-				return ct_aux_search(&((*currNode)->child[i]),(*currNode),newVal);
-			}else{
-				ctNode *nuevo = malloc(sizeof(ctNode));
-				nuevo->father = (*currNode);
-				nuevo->value[0]=0;
-				nuevo->value[1]=0;
-				nuevo->value[2]=0;
-				nuevo->len =1;
-				nuevo->child[0]= NULL;
-				nuevo->child[1]= NULL;
-				nuevo->child[2]= NULL;
-				nuevo->child[3]= NULL;
-				(*currNode)->child[i] = nuevo;
-				return nuevo;
-			}
-
-		}
-		if((*currNode)->value[i]<newVal){
-			if((*currNode)->value[i+1]!=0){
-				return ct_aux_search(&((*currNode)->child[i+1]),(*currNode),newVal);
-			}else{
-				ctNode *nuevo = malloc(sizeof(ctNode));
-				nuevo->father = (*currNode);
-				nuevo->value[0]=0;
-				nuevo->value[1]=0;
-				nuevo->value[2]=0;
-				nuevo->len =1;
-				nuevo->child[0]= NULL;
-				nuevo->child[1]= NULL;
-				nuevo->child[2]= NULL;
-				nuevo->child[3]= NULL;
-				(*currNode)->child[i+1] = nuevo;
-				return nuevo;
-
-			}
-		}
-
-	}*/
+	if(actual->value[2]<newVal){
+		if(actual->child[3]!=NULL)
+			return ct_aux_search(&(actual->child[3]),actual,newVal);
+		ctNode *nuevo = ct_nuevoNodo(actual);
+		actual->child[3]=nuevo;
+		return nuevo;
+	}
 	return NULL;
 }
 
 void ct_aux_fill(ctNode* currNode, uint32_t newVal){
 	//dado un nodo, agrega ordenadamente el valor haciendo los movimientos necesarios y aumentando el tam del nodo
-	for(int i=0;i<2;i++){
-		if(currNode->value[i]<newVal){
-			for(int j=3;j!=i;j--){
-				currNode->value[j-1]=currNode->value[j-2];
-				currNode->child[j]=currNode->child[j-1];
-			}
-			currNode->child[i+1]=currNode->child[i];
-			currNode->value[i]=newVal;
+	if(currNode->len == 0){
+		currNode->value[0]=newVal;
+		currNode->len++;
+		return;
+	}
+	if(currNode->len ==1){
+		if(currNode->value[0]<newVal){
+			currNode->value[1]=newVal;
+			currNode->len++;
+			return;
 		}
-		if(currNode->value[i]>newVal){
-			for(int j=3;j!=i;j--){
-				currNode->value[j-1]=currNode->value[j-2];
-				currNode->child[j]=currNode->child[j-1];
-			}
-			currNode->value[i]=newVal;
+		currNode->value[1]=currNode->value[0];
+		currNode->value[0]=newVal;
+		currNode->len++;
+		return;
+	}
+	if(currNode->len==2){
+		if(currNode->value[1]<newVal){
+			currNode->value[2]=newVal;
+			currNode->len++;
+			return;
 		}
+		if(currNode->value[0]>newVal){
+			currNode->value[2]=currNode->value[1];
+			currNode->value[1]=currNode->value[0];
+			currNode->value[0]=newVal;
+			currNode->len++;
+			return;
+		}
+		currNode->value[2]=currNode->value[1];
+		currNode->value[1]=newVal;
+		currNode->len++;
+		return;
 	}
 }
 
@@ -113,7 +92,7 @@ ctNode* ct_nuevoNodo(ctNode* padre){
 	nuevo->value[0]=0;
 	nuevo->value[1]=0;
 	nuevo->value[2]=0;
-	nuevo->len =1;
+	nuevo->len =0;
 	nuevo->child[0]= NULL;
 	nuevo->child[1]= NULL;
 	nuevo->child[2]= NULL;
